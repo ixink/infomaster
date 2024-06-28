58f5b233d8066fc46558117a0c3cf190e06b8941
ixink <167510114+ixink@users.noreply.github.com>
2024-06-29 00:30:44 +0600

    Create install.py

---
+++ install.py
@@ -0,0 +1,31 @@
+import subprocess
+
+
+print('''
+     ___        __       __  __           _            
+    |_ _|_ __  / _| ___ |  \/  | __ _ ___| |_ ___ _ __ 
+     | || '_ \| |_ / _ \| |\/| |/ _` / __| __/ _ \ '__|
+     | || | | |  _| (_) | |  | | (_| \__ \ ||  __/ |   
+    |___|_| |_|_|  \___/|_|  |_|\__,_|___/\__\___|_|
+            ______________________________________________
+                              coded by:ixink(Rayhan Ahmed)
+                                  tutorial :t.me/ixiinkhat
+               contact me:linkedin.com/in/rayhan-ahmed-uiu
+                                                   
+''')
+
+
+
+def run_apt_up():
+    try:
+        subprocess.run(["pip3", "install", "platform"], check=True)
+        subprocess.run(["pip3", "install", "requests"], check=True)
+        subprocess.run(["pip3", "install", "BeautifulSoup"], check=True)
+        subprocess.run(["pip3", "install", "re"], check=True)
+        subprocess.run(["sudo", "apt", "update"], check=True)
+        print("Installed!")
+    except subprocess.CalledProcessError:
+        print("Error!")
+
+if __name__ == "__main__":
+    run_apt_up()
